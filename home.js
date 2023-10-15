2f051fa7100a48916b299c38620fa534c4614d43
ricardo18235 <registros18235@gmail.com>
2023-10-15 01:54:45 -0500

    js en el main

--- home.js
+++ home.js
@@ -1,1 +1,25 @@
-console.log(data);
+const contenedorDeCards = document.getElementById("contentCards");
+const arrayDeEventos = data.events;
+
+
+for (let event of arrayDeEventos) {
+    contenedorDeCards.classList.add('d-flex', 'justify-content-around', 'flex-wrap', 'cardaround');
+    const creadorCards = document.createElement("div");
+    creadorCards.classList.add('d-flex', 'justify-content-around', 'flex-wrap');
+    creadorCards.innerHTML = (`
+        <div class="card my-3" style="width: 18rem;">
+    <img src=${event.image}" class="card-img-top" height="191"
+        alt="food fair">
+    <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text text-truncate">${event.description}</p>
+        <div class="d-flex justify-content-between">
+            <p>${event.price}</p>
+            <a href="#" class="btn btn-dark">Details</a>
+        </div>
+    </div>
+</div>
+    </div>`)
+
+    contentCards.appendChild(creadorCards);
+}
