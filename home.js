64b1b6d6609e4d6cf442787084c2ceeaedca4c9a
ricardo18235 <registros18235@gmail.com>
2023-10-15 02:10:14 -0500

    js en el main

--- home.js
+++ home.js
@@ -8,17 +8,17 @@ for (let event of arrayDeEventos) {
     creadorCards.classList.add('d-flex', 'justify-content-around', 'flex-wrap');
     creadorCards.innerHTML = (`
         <div class="card my-3" style="width: 18rem;">
-    <img src=${event.image}" class="card-img-top" height="191"
-        alt="food fair">
-    <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text text-truncate">${event.description}</p>
-        <div class="d-flex justify-content-between">
-            <p>${event.price}</p>
-            <a href="#" class="btn btn-dark">Details</a>
+            <img src=${event.image}" class="card-img-top" height="191"
+            alt="food fair">
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text text-truncate">${event.description}</p>
+                <div class="d-flex justify-content-between">
+                    <p>${event.price}</p>
+                    <a href="#" class="btn btn-dark">Details</a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
     </div>`)
 
     contentCards.appendChild(creadorCards);
